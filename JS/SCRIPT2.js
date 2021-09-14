e8db447101c54435ed2b2a550d50a2bd7f949e52
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 00:32:24 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([-35.31, -72.11], 7);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
