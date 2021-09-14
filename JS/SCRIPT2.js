631dd7d634377cd2b4eae55151bcccb6803d4892
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 00:44:29 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -2,9 +2,11 @@ var map = L.map('map').setView([-35.31, -72.11], 7);
 
 let url = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
 
-L.tileLayer(url, {
+var layer_GoogleTerrain_0 = L.tileLayer(url, {
     opacity: 1.0,
-}).addTo(map);
+});
+
+layer_GoogleTerrain_0.addTo(map);
 
 L.marker([-35.31, -72.11]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
