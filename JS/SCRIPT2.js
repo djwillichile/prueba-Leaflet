0920c8a3904f78357729f3df6ff35d922706ddc4
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 01:01:30 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -8,6 +8,12 @@ var layer_GoogleTerrain_0 = L.tileLayer(url, {
 
 layer_GoogleTerrain_0.addTo(map);
 
+let url = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+var layer_OSMStandard_1 = L.tileLayer(url, {
+    opacity: .6,
+});
+
 L.marker([-35.31, -72.11]).addTo(map)
     .bindPopup('HOLIS.<br> Easily customizable.')
     .openPopup();
