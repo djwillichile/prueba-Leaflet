7be122c35017620fe1a60d8db619f40919d9db81
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 01:37:59 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -15,12 +15,16 @@ var layer_GoogleTerrain_1 = L.tileLayer(url2, {
 layer_OSMStandard_0.addTo(map);
 map.addLayer(layer_GoogleTerrain_1);
 
+var colours = ['#00429d', '#2e59a8', '#4771b2', '#5d8abd', '#73a2c6',
+'#8abccf', '#a5d5d8', '#c5eddf', '#ffffe0', '#ffdec7', '#ffbcaf',
+'#ff9895', '#f4777f', '#e4576b', '#cf3759', '#b41648', '#93003a']
+
 d3.request("data/bioc_06.tif").responseType('arraybuffer').get(
             function (error, tiffData) {
                 let bio = L.ScalarField.fromGeoTIFF(tiffData.response);
 
                 let layer = L.canvasLayer.scalarField(bio, {
-                    color: chroma.scale('YlGnBu').domain(bio.range),
+                    color: chroma.scale(colours).domain(bio.range),
                     opacity: 0.65,
                     inFilter: (v) => v !== 0
                 }).addTo(map);
