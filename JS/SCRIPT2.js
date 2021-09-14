81fb96f90f2eb408bff44a94b4988eada8f84634
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 00:42:02 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -1,8 +1,6 @@
 var map = L.map('map').setView([-35.31, -72.11], 7);
 
-let url = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
-
-L.tileLayer(url, {
+L.tileLayer('https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
     opacity: 1.0,
 }).addTo(map);
 
