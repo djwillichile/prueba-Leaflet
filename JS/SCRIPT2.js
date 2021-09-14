492e8a7af9208e329c4847a68b4fd5c1d9087396
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 00:33:59 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -4,7 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([-35.31, -72.11]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
         
