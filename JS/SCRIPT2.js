7eaba30c8910e65e158a9edada8cc922516ed520
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 01:05:18 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -1,16 +1,16 @@
 var map = L.map('map').setView([-35.31, -72.11], 7);
 
-let url1 = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
+let url = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
 
-var layer_GoogleTerrain_0 = L.tileLayer(url1, {
+var layer_GoogleTerrain_0 = L.tileLayer(url, {
     opacity: 1.0,
 });
 
 layer_GoogleTerrain_0.addTo(map);
 
-let url2 = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
+let url = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-var layer_OSMStandard_1 = L.tileLayer(url2, {
+var layer_OSMStandard_1 = L.tileLayer(url, {
     opacity: .6,
 });
 
