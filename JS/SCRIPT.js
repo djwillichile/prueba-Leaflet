c8e60ee4b174dcd4d0e58d7b6dac74a3b298b0f4
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 00:08:07 -0300

    Update SCRIPT.js

--- JS/SCRIPT.js
+++ JS/SCRIPT.js
@@ -3,6 +3,7 @@ let map = L.map('map').setView([51.505, -0.09], 13);
 map.createPane('pane_GoogleTerrain_0');
 map.getPane('pane_GoogleTerrain_0').style.zIndex = 400;
 let url = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
+
 var layer_GoogleTerrain_0 = L.tileLayer(url, {
     pane: 'pane_GoogleTerrain_0',
     opacity: 1.0,
@@ -10,6 +11,7 @@ var layer_GoogleTerrain_0 = L.tileLayer(url, {
     maxZoom: 28,
     minNativeZoom: 0,
     maxNativeZoom: 20
-}).addTo(map);
-layer_GoogleTerrain_0;
+})
+
+layer_GoogleTerrain_0.addTo(map);
         
