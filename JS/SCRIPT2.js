6472083f21f8f2e0b9ab54f5e2ae276651748914
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 01:16:50 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -1,20 +1,19 @@
 var map = L.map('map').setView([-35.31, -72.11], 7);
 
-let url1 = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
+let url1 = 'https://c.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png';
+let url2 = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
 
-var layer_GoogleTerrain_0 = L.tileLayer(url1, {
-    opacity: 1.0,
+
+var layer_OSMStandard_0 = L.tileLayer(url1, {
+    opacity: 1,
 });
 
-layer_GoogleTerrain_0.addTo(map);
-
-let url2 = 'https://c.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png';
-
-var layer_OSMStandard_1 = L.tileLayer(url2, {
+var layer_GoogleTerrain_1 = L.tileLayer(url2, {
     opacity: .6,
 });
 
-map.addLayer(layer_OSMStandard_1);
+layer_OSMStandard_0.addTo(map);
+map.addLayer(layer_GoogleTerrain_1);
 
 L.marker([-35.31, -72.11]).addTo(map)
     .bindPopup('HOLIS.<br> Easily customizable.')
