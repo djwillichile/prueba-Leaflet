e7f7e5cc6c01d11b4a469a0689b9097165138015
djwillichile <guillermo.f1990@gmail.com>
2021-09-15 00:12:38 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-35.31, -72.11], 7);
+var map = L.map('map');
 
 let url1 = 'https://c.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png';
 let url2 = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
@@ -19,9 +19,20 @@ var colours = ['#00429d', '#2e59a8', '#4771b2', '#5d8abd', '#73a2c6',
 '#8abccf', '#a5d5d8', '#c5eddf', '#ffffe0', '#ffdec7', '#ffbcaf',
 '#ff9895', '#f4777f', '#e4576b', '#cf3759', '#b41648', '#93003a']
 
+var layer_Regin_6 = new L.geoJson(json_Regin_6, {
+            attribution: '',
+            interactive: true,
+            dataVar: 'json_Regin_6',
+            layerName: 'layer_Regin_6',
+            pane: 'pane_Regin_6',
+            onEachFeature: pop_Regin_6,
+            style: style_Regin_6_0,
+        });
+
 d3.request("data/bioc_06.tif").responseType('arraybuffer').get(
     function (error, tiffData){
         let bio = L.ScalarField.fromGeoTIFF(tiffData.response);
+
 
         let layer = L.canvasLayer.scalarField(bio, {
             color: chroma.scale(colours).domain(bio.range),
