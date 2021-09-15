02b3b3e6d9b0dd9069a21990d29274b81507af66
djwillichile <guillermo.f1990@gmail.com>
2021-09-14 23:59:42 -0300

    Update SCRIPT2.js

--- JS/SCRIPT2.js
+++ JS/SCRIPT2.js
@@ -19,10 +19,22 @@ var colours = ['#00429d', '#2e59a8', '#4771b2', '#5d8abd', '#73a2c6',
 '#8abccf', '#a5d5d8', '#c5eddf', '#ffffe0', '#ffdec7', '#ffbcaf',
 '#ff9895', '#f4777f', '#e4576b', '#cf3759', '#b41648', '#93003a']
 
+var layer_Regin_6 = new L.geoJson(json_Regin_6, {
+            attribution: '',
+            interactive: true,
+            dataVar: 'json_Regin_6',
+            layerName: 'layer_Regin_6',
+            pane: 'pane_Regin_6',
+            onEachFeature: pop_Regin_6,
+            style: style_Regin_6_0,
+        });
+
+
+
 d3.request("data/bioc_06.tif").responseType('arraybuffer').get(
     function (error, tiffData){
         let bio = L.ScalarField.fromGeoTIFF(tiffData.response);
-        bio.setSpatialMask(json_Comuna_5);
+        bio.setSpatialMask(layer_Regin_6);
 
         let layer = L.canvasLayer.scalarField(bio, {
             color: chroma.scale(colours).domain(bio.range),
